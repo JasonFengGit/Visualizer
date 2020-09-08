6b6aab914797d60e4812b5d4c2efd72eafcc8f98
JasonFengGit <jasonfen@usc.edu>
2020-09-08 21:11:01 +0800

    up

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"></link>
 
       <div className="App">
-        <BrowserRouter basename={window.location.pathname || ''}>
+        <BrowserRouter basename={'JasonFengGit.github.io/'}>
           <Visualizer></Visualizer>
         </BrowserRouter>
       </div >
