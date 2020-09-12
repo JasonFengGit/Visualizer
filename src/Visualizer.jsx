b316b3c39f5b6fdab10afa51844e99cf54f8c814
JasonFengGit <jasonfen@usc.edu>
2020-09-12 18:11:07 +0800

    changed default page to main

--- src/Visualizer.jsx
+++ src/Visualizer.jsx
@@ -8,7 +8,7 @@ export default class Visualizer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            mode: 'pathfinding',
+            mode: 'main',
             rendering: false,
             algorithms: [],
             currentAlgorithm: null,
