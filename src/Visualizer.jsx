611b3ea3b8abc45b71a6f1a450f98510c1ffb9b2
JasonFengGit <jasonfen@usc.edu>
2020-10-24 01:04:11 +0800

    change github icon url

--- src/Visualizer.jsx
+++ src/Visualizer.jsx
@@ -180,7 +180,7 @@ export default class Visualizer extends Component {
                     </div>
 
                     <a href="https://github.com/JasonFengGit" style={{ marginLeft: "32%" }}>
-                        <img class="githubimg" width="40px" height="40px" style={{ opacity: "0.7 !important"}} alt></img>
+                        <img class="githubimg" src="https://github.com/JasonFengGit/Visualizer/raw/master/src/Github_icon.png" width="40px" height="40px" style={{ opacity: "0.7 !important"}} alt></img>
                     </a>
                 </nav>
 
